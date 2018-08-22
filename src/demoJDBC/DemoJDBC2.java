7adb91b5d80e76e81d250596ba786f3b368825e7
vanessahuhn <vanessa.huhn@gmail.com>
2018-08-22 10:19:02 +0200

    Récupération et affichage des résultats

--- src/demoJDBC/DemoJDBC2.java
+++ src/demoJDBC/DemoJDBC2.java
@@ -19,12 +19,33 @@ public class DemoJDBC2 {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection cn = (Connection) DriverManager.getConnection(url,  user,  pwd);
+			
 			//etape 3 : creation d'un statement
 			Statement st = (Statement) cn.createStatement();
+			
 			//etape 4 : definir notre requete
 			String sql = "SELECT * FROM formation.clients";
+			
 			//etape 5 : execution de la requete
 			ResultSet result = st.executeQuery(sql);
+			
+			//etape 6 : récupérer les résultats
+			int ncVar;
+			String nomcVar;
+			String villeVar;
+			
+			while (result.next()) {
+				//recuperer le "nc"
+				ncVar = result.getInt("nc");
+				//recuperer le "nomc"
+				nomcVar = result.getString("nomC");
+				//recuperer "ville"
+				villeVar = result.getString("ville");
+				
+				//affficher une phrase utilisant les résultats
+				System.out.println("Numero client : " + ncVar + ", nom client : " + nomcVar + ", ville : " + villeVar);
+			}
+			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
