150f87a2b4cd6943b15c8100584f8a0584b18191
vanessahuhn <vanessa.huhn@gmail.com>
2018-08-23 11:45:00 +0200

    modif schéma cinema2

---
+++ SQL/cinema/query2.sql
@@ -0,0 +1,8 @@
+-- Requête 2 : Retrouver la liste des films dont la longueur dépasse 180 min.
+
+-- affiche toutes les valeurs du champ titre
+SELECT titre
+-- de la table film
+FROM film
+-- où la longueur est supérieure à 180 minutes
+WHERE longueur > 180;
